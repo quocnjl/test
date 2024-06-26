2d8842952f0582c935f5f42d0ab5ec824c5cac1f
UK1\quocn <quocn@joblogic.com>
2024-06-26 13:40:11 +0700

    w
    w

---
+++ ClientCode/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Nuget.JobLogicSDK;
+
+if (args == null || args.Length != 2)
+    throw new ArgumentException("Please provide 2 arguments: <delay in seconds> <job name>");
+var allArgs = string.Join(" | ", args);
+
+Console.WriteLine("Hello, " + allArgs);
+await Task.Delay(int.Parse(args[0]) * 1000);
+var sdk = new JobLogicSDK();
+sdk.CreateJob(args[1]);
+
---
+++ Nuget.JobLogicSDK/JobLogicSDK.cs
@@ -0,0 +1,11 @@
+﻿namespace Nuget.JobLogicSDK
+{
+    public class JobLogicSDK
+    {
+        public void CreateJob(string jobName)
+        {
+            Console.WriteLine($"Create Job. with Name = '{jobName}', by invoking api [POST] {Environment.GetEnvironmentVariable("JL_SDK_API")}/tenancy/{Environment.GetEnvironmentVariable("JL_SDK_TENANTID")}/jobs" );
+        }
+
+    }
+}
